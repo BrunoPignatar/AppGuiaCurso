b9338226beeec6ea1002131e17f274b7a274ecae
pig <zemane1312@gmail.com>
2022-10-04 15:42:21 -0300

    começo do ComponentesPrimeiro

--- AppGuiaCurso/AppGuiaCurso/View/ComponentesPrimeiro.xaml.cs
+++ AppGuiaCurso/AppGuiaCurso/View/ComponentesPrimeiro.xaml.cs
@@ -7,14 +7,90 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using AppGuiaCurso.Model;
+
 namespace AppGuiaCurso.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ComponentesPrimeiro : ContentView
+    public partial class ComponentesPrimeiro : ContentPage
     {
         public ComponentesPrimeiro()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Linguagem, Trabalho e tecnologia",
+
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e" +
+                                                   "elaborar registros e planilhas de acompanhamento e controle de atividades",
+
+                    ValoresAtitudes = "Incentivar o diálogo e a interlocução. *Responsabiliza-se pela produção, utilização e " +
+                                      "divulgação de informações. *Estimular a proatividade. ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Programação e algoritmos",
+
+                    AtribuicoesResponsabilidades = "criar projetos usando a linguagem de programação",
+                                                 
+
+                    ValoresAtitudes = "Estimular a organização. *incentivar atitudes de autonomia",
+                                     
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_7(object sender, EventArgs e)
+        {
+
         }
     }
 }
